a5495513c6727b7ea496af9cac3f472d9bf3d207
LeeMong <izpzgi2019@gmail.com>
2023-01-07 00:08:47 +0900

    로그인 여부 체크 기능 수정

--- src/main/java/com/spring/minip/product/controller/ProductController.java
+++ src/main/java/com/spring/minip/product/controller/ProductController.java
@@ -162,7 +162,7 @@ public class ProductController {
         // 1. 세션을 얻어서(false는 session이 없어도 새로 생성하지 않는다. 반환값 null)
         HttpSession session = request.getSession(false);
         // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
-        return session!=null && session.getAttribute("id")!=null;
+        return session!=null && session.getAttribute("memberInfo")!=null;
     }
 
 }
